0bde3880ac5eba78bc8958f970a2f01db539766f
Shelby Doolittle <shelby@shelbyd.com>
2021-05-23 13:05:51 -0400

    quit on 'q'

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,65 @@
 use std::{error::Error, io};
-use termion::raw::IntoRawMode;
+use termion::{
+    event::{Event, Key},
+    input::TermRead,
+    raw::IntoRawMode,
+};
 use tui::{
     layout::Alignment,
     style::{Color, Modifier, Style},
     text::{Span, Spans},
-    widgets::{Block, Paragraph, Wrap},
+    widgets::{Block, Paragraph, Widget, Wrap},
 };
 
+struct App;
+
+impl App {
+    fn render(&self) -> impl Widget {
+        let text = vec![
+            Spans::from(vec![
+                Span::raw("First"),
+                Span::styled("line", Style::default().add_modifier(Modifier::ITALIC)),
+                Span::raw("."),
+            ]),
+            Spans::from(Span::styled("Second line", Style::default().fg(Color::Red))),
+        ];
+        Paragraph::new(text)
+            .block(
+                Block::default()
+                    .title("Paragraph")
+                    .borders(tui::widgets::Borders::ALL),
+            )
+            .style(Style::default().fg(Color::White).bg(Color::Black))
+            .alignment(Alignment::Center)
+            .wrap(Wrap { trim: true })
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
+    let stdin = io::stdin();
+    let mut events = stdin.events();
+
     let stdout = io::stdout().into_raw_mode()?;
     let stdout = termion::input::MouseTerminal::from(stdout);
     let backend = tui::backend::TermionBackend::new(stdout);
     let mut terminal = tui::Terminal::new(backend)?;
 
-    for _ in 0..10 {
+    let mut app = App;
+
+    loop {
         terminal.draw(|f| {
             let size = f.size();
-            let text = vec![
-                Spans::from(vec![
-                    Span::raw("First"),
-                    Span::styled("line", Style::default().add_modifier(Modifier::ITALIC)),
-                    Span::raw("."),
-                ]),
-                Spans::from(Span::styled("Second line", Style::default().fg(Color::Red))),
-            ];
-            let p = Paragraph::new(text)
-                .block(
-                    Block::default()
-                        .title("Paragraph")
-                        .borders(tui::widgets::Borders::ALL),
-                )
-                .style(Style::default().fg(Color::White).bg(Color::Black))
-                .alignment(Alignment::Center)
-                .wrap(Wrap { trim: true });
-
-            f.render_widget(p, size)
+            f.render_widget(app.render(), size)
         })?;
-        std::thread::sleep(std::time::Duration::from_millis(100));
+
+        let event = match events.next() {
+            Some(e) => e,
+            None => break,
+        };
+        match event? {
+            Event::Key(Key::Char('q')) => break,
+            _ => {}
+        }
     }
 
     Ok(())
