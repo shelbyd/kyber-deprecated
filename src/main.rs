4584494957c0757a1891dc782293d49c44d0e60c
Shelby Doolittle <shelby@shelbyd.com>
2021-05-23 12:49:59 -0400

    render some text

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use std::{error::Error, io};
 use termion::raw::IntoRawMode;
+use tui::{
+    layout::Alignment,
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{Block, Paragraph, Wrap},
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let stdout = io::stdout().into_raw_mode()?;
@@ -10,10 +16,25 @@ fn main() -> Result<(), Box<dyn Error>> {
     for _ in 0..10 {
         terminal.draw(|f| {
             let size = f.size();
-            let block = tui::widgets::Block::default()
-                .title("Block")
-                .borders(tui::widgets::Borders::ALL);
-            f.render_widget(block, size)
+            let text = vec![
+                Spans::from(vec![
+                    Span::raw("First"),
+                    Span::styled("line", Style::default().add_modifier(Modifier::ITALIC)),
+                    Span::raw("."),
+                ]),
+                Spans::from(Span::styled("Second line", Style::default().fg(Color::Red))),
+            ];
+            let p = Paragraph::new(text)
+                .block(
+                    Block::default()
+                        .title("Paragraph")
+                        .borders(tui::widgets::Borders::ALL),
+                )
+                .style(Style::default().fg(Color::White).bg(Color::Black))
+                .alignment(Alignment::Center)
+                .wrap(Wrap { trim: true });
+
+            f.render_widget(p, size)
         })?;
         std::thread::sleep(std::time::Duration::from_millis(100));
     }
