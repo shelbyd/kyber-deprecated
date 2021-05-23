7a71d3a66512cb11192adfa2a74110c9e8f4f53f
Shelby Doolittle <shelby@shelbyd.com>
2021-05-23 12:40:19 -0400

    basic tui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, io};
+use termion::raw::IntoRawMode;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = termion::input::MouseTerminal::from(stdout);
+    let backend = tui::backend::TermionBackend::new(stdout);
+    let mut terminal = tui::Terminal::new(backend)?;
+
+    for _ in 0..10 {
+        terminal.draw(|f| {
+            let size = f.size();
+            let block = tui::widgets::Block::default()
+                .title("Block")
+                .borders(tui::widgets::Borders::ALL);
+            f.render_widget(block, size)
+        })?;
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+
+    Ok(())
 }
