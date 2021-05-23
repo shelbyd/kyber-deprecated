888e403284a623a1dd26a209f68e4da3c1639be4
Shelby Doolittle <shelby@shelbyd.com>
2021-05-23 14:11:33 -0400

    show search text

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ use termion::{
     raw::IntoRawMode,
 };
 use tui::{
-    layout::Alignment,
+    backend::Backend,
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
     style::{Color, Modifier, Style},
     text::{Span, Spans},
-    widgets::{Block, Paragraph, Widget, Wrap},
+    widgets::{Block, Paragraph, Wrap},
+    Frame,
 };
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -18,17 +20,86 @@ enum ControlFlow {
 }
 
 #[derive(Default)]
-struct App {}
+struct FileSearch {
+    fuzzy_text: String,
+}
+
+impl FileSearch {
+    fn render_height(&self) -> u16 {
+        (self.suggestions().len() as u16) + 1
+    }
+
+    fn suggestions(&self) -> Vec<String> {
+        Vec::new()
+    }
+
+    fn event(&mut self, event: Event) -> ControlFlow {
+        match event {
+            Event::Key(Key::Esc) => return ControlFlow::Break,
+            Event::Key(Key::Char(c)) => self.fuzzy_text.push(c),
+            _ => {},
+        }
+
+        ControlFlow::Continue
+    }
+
+    fn render<B: Backend>(&self, frame: &mut Frame<B>, size: Rect) {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Min(0), Constraint::Length(1)])
+            .split(size);
+
+        let search_text = vec![Spans::from(vec![
+            Span::styled(">>> ", Style::default().fg(Color::Blue)),
+            Span::raw(&self.fuzzy_text),
+        ])];
+        let p =
+            Paragraph::new(search_text).style(Style::default().fg(Color::White).bg(Color::Black));
+        frame.render_widget(p, chunks[1]);
+    }
+}
+
+#[derive(Default)]
+struct App {
+    file_search: Option<FileSearch>,
+}
 
 impl App {
     fn event(&mut self, event: Event) -> ControlFlow {
-        match event {
-            Event::Key(Key::Char('q')) => ControlFlow::Break,
-            _ => ControlFlow::Continue,
+        if let Some(search) = self.file_search.as_mut() {
+            match search.event(event) {
+                ControlFlow::Break => {
+                    self.file_search = None;
+                }
+                ControlFlow::Continue => {}
+            }
+        } else {
+            match event {
+                Event::Key(Key::Char('q')) => return ControlFlow::Break,
+                Event::Key(Key::Ctrl('p')) => {
+                    self.file_search = Some(FileSearch::default());
+                }
+                _ => {}
+            }
         }
+
+        ControlFlow::Continue
     }
 
-    fn render(&self) -> impl Widget {
+    fn render<B: Backend>(&self, frame: &mut Frame<B>) {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([
+                Constraint::Min(0),
+                Constraint::Length(
+                    self.file_search
+                        .as_ref()
+                        .map(|s| s.render_height() + 2)
+                        .unwrap_or(0),
+                ),
+            ])
+            .split(frame.size());
+
         let text = vec![
             Spans::from(vec![
                 Span::raw("First"),
@@ -37,15 +108,15 @@ impl App {
             ]),
             Spans::from(Span::styled("Second line", Style::default().fg(Color::Red))),
         ];
-        Paragraph::new(text)
-            .block(
-                Block::default()
-                    .title("Paragraph")
-                    .borders(tui::widgets::Borders::ALL),
-            )
+        let p = Paragraph::new(text)
             .style(Style::default().fg(Color::White).bg(Color::Black))
             .alignment(Alignment::Center)
-            .wrap(Wrap { trim: true })
+            .wrap(Wrap { trim: true });
+        frame.render_widget(p, chunks[0]);
+
+        if let Some(search) = self.file_search.as_ref() {
+            search.render(frame, chunks[1]);
+        }
     }
 }
 
@@ -62,8 +133,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     loop {
         terminal.draw(|f| {
-            let size = f.size();
-            f.render_widget(app.render(), size)
+            app.render(f);
         })?;
 
         let event = match events.next() {
